53b7bcf9ba41e67957c10cdf5b41f43cc6032037
fabimacchi <fmacchi@consultoriaglobal.com.ar>
2021-11-11 06:42:30 -0300

    Segundo commit pero en desarrollo

---
+++ Primeras_funciones.py
@@ -0,0 +1,15 @@
+def mensaje(): #DECLARACION DE LA FUNCION
+
+	print("Estamos aprendiendo Python") #funcion predefinida print
+	print("Estamos aprendiendo instrucciones basicas")
+	print("Poco a poco iremos avanzando")
+
+mensaje() #LLAMADA DE LA FUNCION
+
+mensaje()
+
+print("Ejecutando codigo fuera de funcion")
+
+mensaje()
+
+
